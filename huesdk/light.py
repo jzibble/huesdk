8bb05c0ffa9b8396d2c717040bec19e8a983d8d2
Jesse Zibble <jzibble@adobe.com>
2023-01-18 23:52:54 -0600

    Added extra light fields and fixed light list on groups.

--- huesdk/light.py
+++ huesdk/light.py
@@ -1,6 +1,6 @@
 import json
 
-from huesdk.generics import hexa_to_xy
+from huesdk.generics import hexa_to_xy,rgb_to_xy
 
 
 class Light:
@@ -19,6 +19,9 @@ class Light:
             self.bri = kwargs['state'].get('bri', None)
             self.hue = kwargs['state'].get('hue', None)
             self.sat = kwargs['state'].get('sat', None)
+            self.colormode = kwargs['state'].get('colormode', None)
+            self.ct = kwargs['state'].get('ct', None)
+            self.xy = kwargs['state'].get('xy', [])
 
     def _put(self, body):
         response = self.sdk.put(uri=f'/{self.sdk.username}/lights/{self.id_}', body=json.dumps(body))
@@ -49,6 +52,33 @@ class Light:
             xy = hexa_to_xy(hexa)
             self._put_state({"xy": xy, "transitiontime": transition})
 
+    def set_states(self, hue=None, bri=None, sat=None, xy=None, ct=None, colormode=None):
+        states = {}
+        if hue and hue != self.hue:
+            states["hue"] = hue
+
+        if sat and sat != self.sat:
+            states["sat"] = sat
+        
+        if bri and bri != self.bri:
+            states["bri"] = bri
+
+        if xy:
+            states["xy"] = xy
+
+        if colormode and colormode != self.colormode:
+            states["colormode"] = colormode
+
+        if ct and ct != self.ct:
+            states["ct"] = ct
+
+        if states:
+            self._put_state(states)
+
     def set_name(self, name):
         self._put({"name": name})
         self.name = name
+
+    def set_colormode(self, colormode):
+        self._put({"colormode": colormode})
+        self.colormode = colormode
